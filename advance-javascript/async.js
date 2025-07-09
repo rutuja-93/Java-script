4d6016595cb01e8af681aead4f7e2582cd77cb52
rutuja-93 <rutujazaware87@gmail.com>
2025-07-09 13:48:17 +0530

    Asynchronous javascript

---
+++ advance-javascript/async.js
@@ -0,0 +1,26 @@
+// setTimeout = Executes a function once after a delay.
+
+
+
+console.log("A");
+
+setTimeout(()=>{
+    console.log("B");
+}, 3000);
+
+console.log("C")
+
+
+
+// setInterval = Repeats a function execute until stopped.
+
+
+console.log("Task 1");
+
+setInterval(()=>{
+    console.log("Task 2");
+}, 3000);
+
+console.log("Task 3");
+
+
